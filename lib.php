a0ddf6792f0799f55fdbd115408ba97a7056ec7d
Jacques Monnard <jacques.monnard@unifr.ch>
2019-08-20 14:57:55 +0200

    New icon for course copy request

--- lib.php
+++ lib.php
@@ -202,7 +202,7 @@ function theme_boost_unifr_reset_app_cache() {
 
 function theme_boost_unifr_extend_navigation_course($navigation, $course, $context) {
     if (has_capability('moodle/course:update', context_course::instance($course->id))) {
-        $coursecopynode = navigation_node::create(get_string('ask_for_copy', 'local_coursecopy'),$url = new moodle_url('/local/coursecopy/archives_request.php', array('id'=>$course->id)), navigation_node::TYPE_CUSTOM, null, null,null);
+        $coursecopynode = navigation_node::create(get_string('ask_for_copy', 'local_coursecopy'),$url = new moodle_url('/local/coursecopy/archives_request.php', array('id'=>$course->id)), navigation_node::TYPE_CUSTOM, null, null,new pix_icon('t/copy', ''));
         //print_r($navigation);
         $navigation->add_node($coursecopynode,'import');
     }
